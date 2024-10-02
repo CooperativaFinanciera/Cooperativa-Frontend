bfe0ac4cfb995cc56c00a85e0c236e5194316691
Richard Javier Cruz Pazmiño <rjcp142000@hotmail.com>
2024-10-02 00:05:04 -0500

    Se actualizo el diseño de la Página de Beneficios Sociales se creo la carpeta assets y la subcarpeta image, se creo la página cuentas y se realizo el encabezado, cuerpo  y pie de página

--- app/SocialBenefits/page.tsx
+++ app/SocialBenefits/page.tsx
@@ -1,53 +1,81 @@
-import React from 'react';
-
+import { HiArrowUpCircle } from "react-icons/hi2";
+import { AuroraBackground } from "@/components/ui/AuroraBackground";
+import { FadeInEffect } from "@/components/ui/FadeInEffect";
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+import benefiImage from '@/app/assets/image/benefi.png';
+import socilImage from '@/app/assets/image/socil.png';
 const SocialBenefitsPage: React.FC = () => {
   return (
-    <div className="w-full mx-auto p-6 text-center bg-gray-50">
-      {/* Encabezado */}
-      <header className="bg-cover bg-center py-16 rounded-lg" style={{ backgroundImage: 'url(/images/banner.jpg)' }}>
-        <h1 className="text-4xl font-bold mb-4 text-black">Conocerán sobre los Beneficios Sociales</h1>
-        <p className="text-xl text-black">Obtén beneficios con esta institución financiera de confianza</p>
-      </header>
+    <>
+      <main className="space-y-10 md:space-y-20">
+        {/* Encabezado */}
+        <header className="p-8 bg-white dark:bg-gray-800 rounded-2xl shadow-lg hover:scale-105 transform transition duration-300 text-center rounded-lg ">
+          <h1 className="text-xl font-bold mb-2 text-gray-900 dark:text-gray-100">
+            Beneficios Sociales
+          </h1>
+          <p className="text-xl font-bold mb-2 text-gray-900 dark:text-gray-100">
+            Descubre los beneficios que ofrecemos para mejorar tu calidad de vida.
+          </p>
+        </header>
+        {/* Sección de Beneficios */}
+        <FadeInEffect className="flex flex-col items-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 w-full max-w-4xl">
+            {/* Tarjeta de Beneficio 1 */}
+            <div className="p-8 bg-white dark:bg-gray-800 rounded-2xl shadow-lg hover:scale-105 transform transition duration-300">
+              <h3 className="text-xl font-bold mb-2 text-gray-900 dark:text-gray-100">Crédito y Ahorro</h3>
+              <h4 className="text-x1 font-bold mb-2 text-gray-900 dark:text-gray-100">Acceso a Créditos Favorables:</h4>
+              <p className="text-gray-700 dark:text-gray-300">
+              Ofrecerán tasas de interés más bajas en préstamos que los bancos tradicionales. Esto permite a los asociados obtener financiamiento a condiciones más favorables.
+              </p>
+              <h4 className="text-x1 font-bold mb-2 text-gray-900 dark:text-gray-100">Políticas de Ahorro Flexibles:</h4>
+              <p className="text-gray-700 dark:text-gray-300">
+              Diseñarán opciones de ahorro que se adapten a las necesidades individuales, como cuentas de ahorro con beneficios adicionales.
+              </p>
+            </div>
 
-      {/* Sección de Beneficios */}
-      <section className="flex flex-col md:flex-row justify-around my-16 text-black">
-        {/* Tarjeta de Crédito y Ahorro */}
-        <div className="p-8 rounded-lg shadow-lg md:w-1/3 mb-8 md:mb-0">
-          <h2 className="text-2xl font-bold text-blue-900 mb-4">Crédito y Ahorro:</h2>
-          <ul className="list-disc list-inside text-left">
-            <li><strong>Acceso a Créditos Favorables:</strong> Ofrecerán tasas de interés más bajas en préstamos que los bancos tradicionales. Esto permite a los asociados obtener financiamiento a condiciones más favorables.</li>
-            <li><strong>Políticas de Ahorro Flexibles:</strong> Opciones de ahorro adaptadas a tus necesidades.</li>
-          </ul>
-        </div>
+            {/* Tarjeta de Beneficio 2 */}
+            <div className="p-8 bg-white dark:bg-gray-800 rounded-2xl shadow-lg hover:scale-105 transform transition duration-300">
+              <h3 className="text-xl font-bold mb-2 text-gray-900 dark:text-gray-100">Bienestar y Salud</h3>
+              <h4 className="text-x1 font-bold mb-2 text-gray-900 dark:text-gray-100">Seguros Asequibles:</h4>
+              <p className="text-gray-700 dark:text-gray-300">
+              Proporcionarán seguros y pólizas a precios accesibles para proteger a los asociados y sus familias.
+              </p>
+              <h4 className="text-x1 font-bold mb-2 text-gray-900 dark:text-gray-100">Programas de Bienestar:</h4>
+              <p className="text-gray-700 dark:text-gray-300">
+              Diseñarán iniciativas que promuevan la salud, la alimentación saludable y el cuidado personal.
+              </p>
+            </div>
+          </div>
+        </FadeInEffect>
 
-        {/* Tarjeta de Bienestar y Salud */}
-        <div className="p-8 rounded-lg shadow-lg md:w-1/3">
-          <h2 className="text-2xl font-bold text-blue-900 mb-4">Bienestar y Salud:</h2>
-          <ul className="list-disc list-inside text-left">
-            <li><strong>Seguros Asequibles:</strong> Seguros y pólizas a precios accesibles.</li>
-            <li><strong>Programas de Bienestar:</strong> Iniciativas que promueven la salud y el bienestar.</li>
-          </ul>
-        </div>
-      </section>
+        {/* Sección de Imágenes */}
+        <FadeInEffect className="flex flex-col items-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-4xl">
+            <img
+              className="w-full h-64 object-cover rounded-2xl shadow-lg"
+              src={benefiImage.src}
+              alt="Tarjeta de crédito"
+            />
+            <img
+              className="w-full h-64 object-cover rounded-2xl shadow-lg"
+              src={socilImage.src}
+              alt="Cuidado de la salud"
+            />
+          </div>
+        </FadeInEffect>
 
-      {/* Sección de Imágenes */}
-      <section className="flex flex-col md:flex-row justify-around my-16 space-y-8 md:space-y-0 md:space-x-8">
-        <div>
-          <img className="w-full h-64 object-cover rounded-lg text-black" src="/images/credit-card.jpg" alt="Tarjeta de crédito" />
-        </div>
-        <div>
-          <img className="w-full h-64 object-cover rounded-lg text-black" src="/images/health-care.jpg" alt="Cuidado de la salud" />
-        </div>
-      </section>
-
-      {/* Pie de Página */}
-      <footer className="bg-blue-900 text-white py-8">
-        <div className="text-center">
-          <p className="text-lg mb-2">Profesionales en finanzas, ofreciendo asesoría de calidad.</p>
-          <p className="text-sm">Contáctanos: <strong>+593 0981321044</strong></p>
-        </div>
-      </footer>
-    </div>
+        {/* Pie de Página */}
+        <footer className="p-8 bg-white dark:bg-gray-800 rounded-2xl shadow-lg hover:scale-105 transform transition duration-300 rounded-lg">
+          <div className="text-center text-xl font-bold mb-2 text-gray-900 dark:text-gray-100">
+            <p className="text-lg font-bold">
+              Mejora tu futuro financiero con nuestros beneficios exclusivos.
+            </p>
+            <p className="text-sm">Contáctanos: +593 0981321044</p>
+          </div>
+        </footer>
+      </main>
+    </>
   );
 };
 
