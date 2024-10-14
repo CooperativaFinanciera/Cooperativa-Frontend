e5ebcbb85080b5a4f4dd1d858fd9b3edd95562cc
hamil <hamiltonxb3.3@gmail.com>
2024-10-14 08:19:25 -0500

    creditos add components

--- app/articles/creditos/page.tsx
+++ app/articles/creditos/page.tsx
@@ -26,16 +26,35 @@ export default function creditos(){
                         priority
                     />
                 </div>
-            <div className="relative w-full h-64">
-
-            </div>
             <div className="grid grid-cols-5">
                 <div></div>
-                <div className="col-span-3 space-y-10 md:space-y-20 bg-secondary">
-
+                <div className="col-span-3 space-y-10 md:space-y-20 flex gap-6">
+                    <Card className="bg-primary text-white p-6 rounded-2xl shadow-md border-2 border-white text-center w-1/2 max-w-sm">
+                        <CardBody>
+                            <h3 className="text-lg font-bold">CARACTERISTICAS</h3>
+                            <ul className="mt-4 space-y-2 text-sm">
+                                <li>* MONTOS: Desde USD 1,000 hasta USD 20,000</li>
+                                <li>* PLAZO MAXIMO: 60 meses</li>
+                                <li>* TASA: 15.60%</li>
+                                <li>* SIN GARANTE</li>
+                                <li>* SIN COSTOS ADICIONALES</li>
+                                <li>« Aplican condiciones.</li>
+                            </ul>
+                        </CardBody>
+                    </Card>
+                    
+                    <Card className=" w-full max-w-lg">
+                        <CardBody>
+                            <h2 className="text-xl font-semibold mb-4">Crédito a tu medida</h2>
+                            <p className="text-sm">
+                                Accederás a futuro al crédito que se ajusta a ti.
+                            </p>
+                        </CardBody>
+                    </Card>
                 </div>
                 <div></div>
             </div>
+
         </main>
         </>
     )
