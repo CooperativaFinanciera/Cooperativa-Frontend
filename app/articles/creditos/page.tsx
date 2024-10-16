b14272f1bfbcccc91feebe68b9ecc31eb36ec31e
hamil <hamiltonxb3.3@gmail.com>
2024-10-16 11:42:16 -0500

    ending creditos

--- app/articles/creditos/page.tsx
+++ app/articles/creditos/page.tsx
@@ -21,71 +21,56 @@ export default function Creditos() {
                         priority
                     />
                 </div>
-                <div className="grid-cols-5">
-                <div></div>
-                <div className="col-span-3 space-y-10 md:space-y-20 flex gap-6">
-                    <div className="col-span-1 space-y-10 md:space-y-20 flex gap-6">
-                    <h2 className="text-xl font-semibold mb-2">CREDITO ANEUPI</h2>  
-                    </div>
-                    <div className="col-span-2 space-y-10 md:space-y-20 flex gap-6"></div>
-                </div>
-                <div></div>
-                </div>
-                <div className="grid grid-cols-5">
-                    <div></div>
-                    
-                    <div className="col-span-3 space-y-10 md:space-y-20 flex gap-6">
-                        {/* Primera Card */}
-                        <div className="w-1/2 max-w-sm text-center">
-                            
-                            <Card className="bg-primary text-white p-6 rounded-2xl shadow-md border-2 border-white">
-                                <CardBody>
-                                    <h3 className="text-lg font-bold">CARACTERISTICAS</h3>
-                                    <ul className="mt-4 space-y-2 text-sm">
-                                        <li>* MONTOS: Desde USD 1,000 hasta USD 20,000</li>
-                                        <li>* PLAZO MAXIMO: 60 meses</li>
-                                        <li>* TASA: 15.60%</li>
-                                        <li>* SIN GARANTE</li>
-                                        <li>* SIN COSTOS ADICIONALES</li>
-                                        <li>« Aplican condiciones.</li>
-                                    </ul>
-                                </CardBody>
-                            </Card>
-                        </div>
 
-                        {/* Segunda Card */}
-                        <div className="w-full max-w-lg text-center">
-                            <h2 className="text-xl font-semibold mb-2">¿Qué tienes en mente?</h2>
-                            <p className="text-sm mb-4">Le ayudamos a cumplir sus sueños</p>
-                            <Button className="mt-6" onPress={() => setVisible(true)}>Solicítelo aquí</Button>
-                            <Card>
-                                <CardBody>
-                                    <h2 className="text-xl font-semibold mb-4">Crédito a tu medida</h2>
-                                    <p className="text-sm mb-6">
-                                        Accederás a futuro al crédito que se ajusta a ti.
-                                    </p>
-                                    <div className="flex justify-between">
-                                        {[
-                                            { icon: "💳", title: "Crédito Consumo (En desarrollo)", description: "No te detengas. Para tus necesidades de financiamiento, tenemos la solución inmediata" },
-                                            { icon: "🚗", title: "Crédito Vehicular (En desarrollo)", description: "Empieza el camino hacia tu nuevo vehículo. Nosotros, lo financiamos hasta 60 meses plazo." },
-                                            { icon: "🏠", title: "Crédito Hipotecario (En desarrollo)", description: "Materializa el sueño de una vivienda propia. Financiamos la construcción, compra o ampliación de tu nueva casa." }
-                                        ].map((item, index) => (
-                                            <div key={index} className="flex flex-col items-center text-center space-y-2">
-                                                <div className="flex items-center justify-center w-12 h-12 rounded-full bg-gray-100">
-                                                    <span className="text-2xl">{item.icon}</span>
-                                                </div>
-                                                <h3 className="text-base font-semibold">{item.title}</h3>
-                                                <p className="text-sm text-gray-500">{item.description}</p>
-                                            </div>
-                                        ))}
-                                    </div>
-                                    {/* Botón para abrir el modal */}
-                                    
-                                </CardBody>
-                            </Card>
-                        </div>
+                {/* Contenedor principal */}
+                <div className="flex justify-center space-x-6">
+                    {/* Contenedor del primer conjunto */}
+                    <div className="w-1/3 max-w-sm text-center flex flex-col">
+                        <h2 className="text-xl text-left font-semibold mb-4">CREDITO ANEUPI</h2>
+                        <Card className="bg-primary text-white p-6 rounded-2xl shadow-md border-2 border-white flex-grow">
+                            <CardBody>
+                                <h3 className="text-lg font-bold">CARACTERISTICAS (En desarrollo)</h3>
+                                <ul className="mt-4 space-y-2 text-sm">
+                                    <li>* MONTOS: Desde USD 1,000 hasta USD 20,000</li>
+                                    <li>* PLAZO MAXIMO: 60 meses</li>
+                                    <li>* TASA: 15.60%</li>
+                                    <li>* SIN GARANTE</li>
+                                    <li>* SIN COSTOS ADICIONALES</li>
+                                    <li>« Aplican condiciones.</li>
+                                </ul>
+                            </CardBody>
+                        </Card>
                     </div>
-                    <div></div>
+
+                    {/* Contenedor del segundo conjunto */}
+                    <div className="w-1/3 max-w-lg text-center flex flex-col">
+                        <h2 className="text-xl  text-left font-semibold mb-2">¿Qué tienes en mente?</h2>
+                        <p className="text-sm mb-2 text-left">Le ayudamos a cumplir sus sueños</p>
+                        <Button onPress={() => setVisible(true)} className="mb-4">Solicítelo aquí</Button>
+                        <Card className="flex-grow">
+                            <CardBody>
+                                <h2 className="text-xl font-semibold mb-4">Crédito a tu medida</h2>
+                                <p className="text-sm mb-6">
+                                    Accederás a futuro al crédito que se ajusta a ti.
+                                </p>
+                                <div className="flex justify-between">
+                                    {[
+                                        { icon: "💳", title: "Crédito Consumo (En desarrollo)", description: "Para tus necesidades de financiamiento, tenemos la solución inmediata." },
+                                        { icon: "🚗", title: "Crédito Vehicular (En desarrollo)", description: "Financiamos hasta 60 meses para tu nuevo vehículo." },
+                                        { icon: "🏠", title: "Crédito Hipotecario (En desarrollo)", description: "Financiamos la construcción, compra o ampliación de tu nueva casa." }
+                                    ].map((item, index) => (
+                                        <div key={index} className="flex flex-col items-center text-center space-y-2">
+                                            <div className="flex items-center justify-center w-12 h-12 rounded-full bg-gray-100">
+                                                <span className="text-2xl">{item.icon}</span>
+                                            </div>
+                                            <h3 className="text-base font-semibold">{item.title}</h3>
+                                            <p className="text-sm text-gray-500">{item.description}</p>
+                                        </div>
+                                    ))}
+                                </div>
+                            </CardBody>
+                        </Card>
+                    </div>
                 </div>
 
                 {/* Modal */}
